985398615c9ff78fd0bcbcae17cf9446caa25fa0
Vagiza <w.svidchenkova@gmail.com>
2021-10-04 21:44:31 +0300

    Создала компоненты остальных страниц

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,7 +1,11 @@
 import Main from '../main/main';
 
-function App(): JSX.Element {
-  return <Main />;
+type AppProps = {
+  placeCount: number;
+}
+
+function App({placeCount}: AppProps): JSX.Element {
+  return <Main placeCount={placeCount} />;
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  PLACE_COUNT: 5,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App placeCount={Setting.PLACE_COUNT} />
   </React.StrictMode>,
   document.getElementById('root'));
